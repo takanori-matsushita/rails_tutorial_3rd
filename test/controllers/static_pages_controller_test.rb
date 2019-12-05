e0fee3164fcd26752dced5ccbbd37ee6e77a044c
松下貴紀 <t.matsushita@arcjobz.com>
2019-12-05 16:56:20 +0900

    contact追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -13,6 +18,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get about" do
     get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
   end
 end
