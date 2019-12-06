2e4b1c6091c0ea4a17f49aebb3b7683692c18810
松下貴紀 <t.matsushita@arcjobz.com>
2019-12-07 01:08:43 +0900

    fix_routes,部分テンプレート作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,23 +6,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
   end
 end
