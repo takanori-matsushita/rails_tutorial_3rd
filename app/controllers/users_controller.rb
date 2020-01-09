9885ef5d59788acb57b3ffeae6e301fb8beb9318
松下貴紀 <t.matsushita@arcjobz.com>
2020-01-10 01:11:00 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to(root_url) and return unless @user.activated
   end
 
@@ -45,17 +46,10 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please login"
-        redirect_to login_path
-      end
     end
 
     def correct_user
